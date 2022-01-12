813cde1525831c454c68150e8005dc67e5e09fa2
Daniel Lawless <danlawless1999@gmail.com>
2022-01-12 17:15:13 +1000

    Basic Three.js Content Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
@@ -30,14 +31,32 @@ pointLight.position.set(20,20,20);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight,ambientLight)
 
+const controls = new OrbitControls(camera,renderer.domElement)
+
 function animate(){
   requestAnimationFrame(animate);
   torus.rotation.x += .01;
   torus.rotation.y += .005;
-
   torus.rotation.z += .01;
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(.25,24,24);
+  const material = new THREE.MeshStandardMaterial({color:0xffffff});
+  const star = new THREE.Mesh(geometry,material);
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+
+}
+
+Array(200).fill().forEach(addStar);
+
 
 animate();
